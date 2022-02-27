bdb980c96a5c55c29c474680cb758f455105a3ba
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-02-27 19:18:37 +0800

    fix: db connection on both dev and prod

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const pgConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: isProduction ? { rejectUnauthorized: false } : undefined,
   entities: ['dist/**/*.entity.js'],
   migrationsRun: isProduction,
   migrations: ['src/migrations/**/*{.ts,.js}'],
