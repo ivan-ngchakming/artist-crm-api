1e3f5a8c60d1dd2391e374dac449cfeb2cf9d217
ivan0313 <ivan0313@connect.hku.hk>
2022-02-27 14:25:12 +0800

    fix: fixed addresses unittest issues

--- src/addresses/addresses.service.spec.ts
+++ src/addresses/addresses.service.spec.ts
@@ -14,13 +14,22 @@ describe('AddressesService', () => {
   const mockRepository = {
     find: () => mockAddresses,
     findOne: (id: number) => mockAddresses.find((address) => address.id === id),
-    create: (obj) => obj,
-    save: (obj) => [{ id: mockAddresses.length, ...obj }, ...mockAddresses],
+    create: (obj) => {
+      const newAddrObj = { id: mockAddresses.length + 1, ...obj };
+      mockAddresses.push(newAddrObj);
+      return newAddrObj;
+    },
     update: (id, obj) => {
-      mockAddresses = {
-        ...mockAddresses.find((address) => address.id === id),
-        ...obj,
-      };
+      mockAddresses = [
+        ...mockAddresses.filter((addr) => addr.id !== id),
+        {
+          ...mockAddresses.find((address) => address.id === id),
+          ...obj,
+        },
+      ];
+    },
+    delete: (id) => {
+      mockAddresses = mockAddresses.filter((addr) => addr.id !== id);
     },
   };
 
@@ -36,6 +45,11 @@ describe('AddressesService', () => {
     }).compile();
 
     service = module.get<AddressesService>(AddressesService);
+
+    mockAddresses = [
+      { id: 1, lineOne: 'addr 1 line one', lineTwo: 'addr 1 line two' },
+      { id: 2, lineOne: 'addr 2 line one', lineTwo: 'addr 2 line two' },
+    ];
   });
 
   it('should be defined', () => {
@@ -55,33 +69,27 @@ describe('AddressesService', () => {
       lineOne: 'new addr line 1',
       lineTwo: 'new addr line 2',
     };
-    service
-      .create(addressToBeCreated)
-      .then((data) => {
-        expect(data).toContain(expect.objectContaining(addressToBeCreated));
-      })
-      .catch((error) => console.error(error));
+
+    return service.create(addressToBeCreated).then((data) => {
+      expect(data).toEqual(expect.objectContaining(addressToBeCreated));
+    });
   });
 
   it('should update address', () => {
-    service
-      .update(2, { lineOne: 'new addr line 1' })
+    return service
+      .update(2, { lineOne: 'updated address line 1' })
       .then((data) => {
-        expect(data).toContain(
+        expect(data).toEqual(
           expect.objectContaining({ id: 2, lineOne: 'updated address line 1' }),
         );
-      })
-      .catch((error) => console.error(error));
+      });
   });
 
   it('should delete address', () => {
-    service
-      .remove(2)
-      .then(() => {
-        expect(service.findAll()).toEqual(
-          expect.not.arrayContaining(expect.objectContaining({ id: 2 })),
-        );
-      })
-      .catch((error) => console.error(error));
+    return service.remove(2).then(() => {
+      expect(mockAddresses).toEqual(
+        expect.not.arrayContaining([expect.objectContaining({ id: 2 })]),
+      );
+    });
   });
 });
