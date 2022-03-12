f5da10a0aad63584a8d6b1b440dce381675bb4f6
Ivan Ng <97230812+ivan-ngchakming@users.noreply.github.com>
2022-03-12 17:30:37 +0800

    feat: email get body content (#7)
    * feat: debug nestjs applicaiton
    
    * feat: added get one route

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { ImapFlow, ImapFlowOptions } from 'imapflow';
+import {
+  FetchQueryObject,
+  ImapFlow,
+  ImapFlowOptions,
+  SequenceString,
+  FetchMessageObject,
+} from 'imapflow';
 
 @Injectable()
 export class EmailsService {
@@ -22,29 +28,78 @@ export class EmailsService {
     return this.clients[user];
   }
 
+  sequelizeFetchMessageObject(dataArg: FetchMessageObject) {
+    const data = { ...dataArg };
+
+    for (const key in data) {
+      if (typeof data[key] === 'bigint') {
+        data[key] = data[key].toString();
+      } else if (data[key] instanceof Set) {
+        data[key] = Array.from(data[key]);
+      }
+    }
+
+    if (data.bodyParts) {
+      data['bodyPartsDecoded'] = Array.from(data.bodyParts.values()).map(
+        (val) => val.toString(),
+      );
+    }
+
+    return data;
+  }
+
   /**
    * Return all user's emails
    * TODO: Implement pagination
    */
-  async findAll(config: ImapFlowOptions) {
+  async findAll(
+    config: ImapFlowOptions,
+    range: SequenceString,
+    fetchOptions?: Partial<FetchQueryObject>,
+  ) {
     const client = await this.createClient(config);
     const lock = await client.getMailboxLock('INBOX');
 
     const messages = [];
     try {
-      for await (const message of client.fetch('1:10', {
+      for await (const message of client.fetch(range, {
         flags: true,
         envelope: true,
+        ...fetchOptions,
       })) {
-        messages.push({
-          id: message.emailId,
-          flags: Array.from(message.flags),
-          ...message.envelope,
-        });
+        messages.push(this.sequelizeFetchMessageObject(message));
       }
     } finally {
       lock.release();
     }
     return messages;
+  }
+
+  async findOne(
+    config: ImapFlowOptions,
+    uid: string,
+    fetchOptions?: Partial<FetchQueryObject>,
+  ) {
+    const client = await this.createClient(config);
+    const lock = await client.getMailboxLock('INBOX');
+
+    let message;
+    try {
+      message = await client.fetchOne(
+        uid,
+        {
+          flags: true,
+          envelope: true,
+          bodyStructure: true,
+          ...fetchOptions,
+        },
+        { uid: true },
+      );
+
+      console.log(message);
+    } finally {
+      lock.release();
+    }
+    return this.sequelizeFetchMessageObject(message);
   }
 }
