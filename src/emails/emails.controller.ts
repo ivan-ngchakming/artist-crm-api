0a9ea5fc8e81edf2158d8affd2c0402829d8c0b2
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-03-12 17:28:25 +0800

    feat: added get one route

--- src/emails/emails.controller.ts
+++ src/emails/emails.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Req, UnauthorizedException } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  Get,
+  Req,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { EmailsService } from './emails.service';
 
@@ -6,21 +12,64 @@ import { EmailsService } from './emails.service';
 export class EmailsController {
   constructor(private readonly emailsService: EmailsService) {}
 
+  /**
+   * Sequelize query string to imapflow FetchQueryObject.
+   * TODO: sequelize remaining query params
+   */
+  sequelizeQuery({ bodyParts: bodyPartsQuery, ...queryParams }: any) {
+    let bodyParts = [];
+    if (typeof bodyPartsQuery === 'string') {
+      bodyParts.push(bodyPartsQuery);
+    } else if (bodyPartsQuery) {
+      bodyParts = bodyPartsQuery as string[];
+    }
+    return {
+      bodyParts,
+      ...queryParams,
+    };
+  }
+
   @Get()
   async findAll(@Req() request: Request) {
     const { emailUser, emailPass } = request.cookies;
+    const query = this.sequelizeQuery(request.query);
 
     if (!emailUser || !emailPass) throw new UnauthorizedException();
 
-    return this.emailsService.findAll({
-      auth: {
-        user: emailUser,
-        pass: emailPass,
+    return this.emailsService.findAll(
+      {
+        auth: {
+          user: emailUser,
+          pass: emailPass,
+        },
+        host: 'imap.gmail.com',
+        port: 993,
+        secure: true,
       },
-      host: 'imap.gmail.com',
-      port: 993,
-      secure: true,
-      // logger: false,
-    });
+      '1:1',
+      query,
+    );
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string, @Req() request: Request) {
+    const { emailUser, emailPass } = request.cookies;
+    const query = this.sequelizeQuery(request.query);
+
+    if (!emailUser || !emailPass) throw new UnauthorizedException();
+
+    return this.emailsService.findOne(
+      {
+        auth: {
+          user: emailUser,
+          pass: emailPass,
+        },
+        host: 'imap.gmail.com',
+        port: 993,
+        secure: true,
+      },
+      id,
+      query,
+    );
   }
 }
