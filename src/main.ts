95fb635e33cbdba2246089649f47706cd66cf79d
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-03-25 16:41:11 +0800

    fix: cors allow origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,27 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: [
+      /**
+       * localhost on all ports
+       */
+      /^http:\/\/localhost:?[0-9]*$/,
+      /**
+       * any subdomain of ivan0313.tk
+       */
+      /\.ivan0313\.tk$/,
+      /**
+       * free domain given by netlify
+       */
+      'artist-crm.netlify.app',
+      /**
+       * netlify PR previews
+       */
+      /https:\/\/deploy-preview-[0-9]*--artist-crm\.netlify\.app\//,
+    ],
+  });
   app.use(cookieParser());
 
   await app.listen(process.env.PORT || 8000);
